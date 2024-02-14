88e319eadcb7dfca80f9f2adced0e45bef8d374e
unknown <namitlodh>
2024-02-14 22:48:52 +0530

    [Namit] Add: Select Command

---
+++ Day 1/Update command.sql
@@ -0,0 +1,12 @@
+Update employee_info 
+set empSalary=empSalary+1000;
+
+Select * from employee_info;
+
+Update employee_info 
+SET job='Tester'
+Where empName='sm ith';
+
+Update employee_info
+Set phone='3456312098', deptId=11
+where empId=2;
