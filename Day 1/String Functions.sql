33d01e404573db9da98e27e923611d384d20d84f
unknown <namitlodh>
2024-02-15 00:11:25 +0530

    [Namit] Add: string functions

---
+++ Day 1/String Functions.sql
@@ -0,0 +1,13 @@
+SELECT empName, length=LEN(empName) from employee_info; 
+
+SELECT UPPER('this is upper')
+
+select empName, lower(empName) from employee_info;
+
+SELECT LTRIM('   ANYSTRING'),RTRIM('    ANYSTRING');
+
+SELECT SUBSTRING('MICROSOFT',6,9)
+
+SELECT REPLACE('MICROSOFT','MICRO','MAJOR')
+
+SELECT REPLICATE('DUMMY ',5)
