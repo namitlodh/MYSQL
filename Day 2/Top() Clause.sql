b94eb1cca9a5278df31bdb28ca546aadbd5efc34
unknown <namitlodh>
2024-02-15 15:56:15 +0530

    [Namit] Add: TOP() clause

---
+++ Day 2/Top() Clause.sql
@@ -0,0 +1,9 @@
+SELECT * FROM employee_info;
+
+SELECT empSalary from employee_info order by empSalary desc;
+
+SELECT empSalary from employee_info order by empSalary;
+
+SELECT TOP(3) empSalary from employee_info order by empSalary desc;
+
+SELECT TOP(3) empSalary from employee_info order by empSalary;
