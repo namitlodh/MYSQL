33ee4770863bb5df7878798fb351fa39db61d3d2
unknown <namitlodh>
2024-02-18 15:37:04 +0530

    [Namit] Add: Inner Join

---
+++ Day 4/Inner Join.sql
@@ -0,0 +1,10 @@
+select * from emp_info_bckp;
+select * from department_info;
+
+SELECT empId, empName, empSalary,deptname, deptlocation
+from emp_info_bckp INNER JOIN department_info
+on emp_info_bckp.deptId = department_info.deptId;
+
+SELECT e.empId, e.empName, e.empSalary,d.deptname, d.deptlocation
+from emp_info_bckp e INNER JOIN department_info d
+on e.deptId = d.deptId;
