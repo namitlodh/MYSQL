a1cae5a692e4c8e32b4e377d0e14606d19ec93c0
unknown <namitlodh>
2024-02-19 23:48:43 +0530

    [Namit] Add: Go command in sql

---
+++ Day 5/GO  Command.sql
@@ -0,0 +1,16 @@
+use employeeDB;
+GO
+
+--DECLARE @Name VARCHAR(50);
+--SELECT @Name ='Microsoft';
+--GO
+
+DECLARE @Name VARCHAR(50);
+SELECT @Name ='Microsoft';
+SELECT @Name AS 'NAME';
+GO
+
+DECLARE @Name VARCHAR(50);
+SELECT @Name='GOOGLE';
+SELECT @Name AS 'NAME';
+GO
